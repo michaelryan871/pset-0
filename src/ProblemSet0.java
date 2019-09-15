d87b9dd7b4ac8ba4a9937536bf0806f0b415c1e6
Michael Ryan <mike.ryan871@gmail.com>
2019-09-15 14:30:14 -0400

    Heading Fixed, Exercise 1 & 2 Fixed

--- src/ProblemSet0.java
+++ src/ProblemSet0.java
@@ -1,48 +1,48 @@
 /**
  * Problem Set 0.
- * 
+ *
  * It's time to put your skills to the test. This problem set focuses on printing
  * text to the console, as well as identifying and correcting compilation and
  * runtime errors in the supplied program code.
- * 
+ *
  * The specifications for each exercise are outlined below. I've written most of
  * the functionality for you, but unfortunately, my code is riddled with mistakes.
  * It's your job to fix the errors I've made, so that the code meets the
  * requirements set forth in this section (as well as the Deliverables section).
- * 
+ *
  * Work through these exercises on your own. Experiment, make mistakes, ask
  * questions, and fix your mistakes. It's the only way to get good at programming.
  */
 
-Public class PSet0 {
+public class ProblemSet0 {
 
-    public void Main(String args) {
+    public static void main(String[] args) {
 
         /*
          * Exercise 1.
-         * 
+         *
          * Print a message to the console.
          */
 
-        systemout.println(Hello, world! Am I a programmer yet?\n"
+        System.out.println("\nHello, world! Am I a programmer yet?\n");
 
         /*
          * Exercise 2.
-         * 
+         *
          * Print APCS in block letters to the console.
          */
 
-        System.out.println("   ##    ######  ######## ########")
-        System.out.println("  #  #   #     # #        #";
-        System.out.println(" #    #  #     # #       #");
-        Syste.out.print("######## ######  #        ########");
-        System.out.printf("#      # #       #               #");
+        System.out.println("   ##    ######  ######## ########");
+        System.out.println("  #  #   #     # #        #");
+        System.out.println(" #    #  #     # #        #");
+        System.out.println("######## ######  #        ########");
         System.out.println("#      # #       #               #");
-        Systemoutprintln("#      # #       ######## ########\n");
+        System.out.println("#      # #       #               #");
+        System.out.println("#      # #       ######## ########\n");
 
         /*
          * Exercise 3.
-         * 
+         *
          * Print a quoted message to the console.
          */
 
@@ -50,7 +50,7 @@ Public class PSet0 {
 
         /*
          * Exercise 4.
-         * 
+         *
          * Print a face to the console.
          */
 
@@ -63,7 +63,7 @@ Public class PSet0 {
 
         /*
          * Exercise 5.
-         * 
+         *
          * Print a to-do list to the console.
          */
 
@@ -71,7 +71,7 @@ Public class PSet0 {
 
         /*
          * Exercise 6.
-         * 
+         *
          * Print a square of text to the console.
          */
 
@@ -79,7 +79,7 @@ Public class PSet0 {
 
         /*
          * Exercise 7.
-         * 
+         *
          * Print a countdown to the console.
          */
 
@@ -87,7 +87,7 @@ Public class PSet0 {
 
         /*
          * Exercise 8.
-         * 
+         *
          * Print three diamonds to the console.
          */
 
@@ -100,7 +100,7 @@ Public class PSet0 {
 
         /*
          * Exercise 9.
-         * 
+         *
          * Print an encouraging message to the console.
          */
 
@@ -108,7 +108,7 @@ Public class PSet0 {
 
         /*
          * Exercise 10.
-         * 
+         *
          * Print a plus sign to the console.
          */
 
